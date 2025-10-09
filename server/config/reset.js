a0e2222b19d061522f1671b8b1f5a561f2607509
Bhagyesh Jethwani <jethwani.bhagyesh@gmail.com>
2025-10-09 01:24:32 -0400

    inserted values into database and prevented sql injection

---
+++ server/config/reset.js
@@ -0,0 +1,51 @@
+import pool from './database.js'
+import dotenv from 'dotenv.js'
+import events from '../data/eventData.js'
+
+const createEventsTable = async() => {
+    const createTableQuery = `
+    DROP TABLE IF EXISTS events;
+
+    CREATE TABLE IF NOT EXISTS events(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        date VARCHAR(50) NOT NULL,
+        length VARCHAR(50) NOT NULL,
+        image VARCHAR(50) NOT NULL
+    )
+`
+    try{
+        const res = pool.query(createTableQuery)
+        console.log('table created')
+    }
+    catch(err)
+    {
+        console.error('error creating table')
+    }
+}
+
+const seedEventsTable = async() => {
+    await createEventsTable()
+    events.forEach(event => {
+        const query = 
+        {
+            text: `INSERT INTO events (name, date, length, image) VALUES ($1,$2,$3,$4)`
+        }
+        const values = [
+            event.name,
+            event.date,
+            event.length,
+            event.image
+        ]
+        pool.query(text, values, (req, res) => {
+            if(err)
+            {
+                console.error('error inserting values')
+            }
+            console.log(`${event.name} added`)
+        })
+    })
+}
+
+seedEventsTable()
+
---
+++ server/data/eventData.js
@@ -0,0 +1,39 @@
+const events = [
+    {
+        id: 1,
+        name: 'swift',
+        date: "may 5 2025",
+        length: '5 months 3 days',
+        image: "https://as1.ftcdn.net/v2/jpg/07/46/85/18/1000_F_746851897_vIKiiA6EqPxuDiUbH6HQ35rBnCNIa7WO.jpg"
+    },
+    {
+        id: 2,
+        name: 'drake',
+        date: "may 7 2025",
+        length: '6 months 10 days',
+        image: "https://as1.ftcdn.net/v2/jpg/07/46/85/18/1000_F_746851897_vIKiiA6EqPxuDiUbH6HQ35rBnCNIa7WO.jpg"
+    },
+    {
+        id: 3,
+        name: 'eminem',
+        date: "june 5 2025",
+        length: '4 months 3 days',
+        image: "https://as1.ftcdn.net/v2/jpg/07/46/85/18/1000_F_746851897_vIKiiA6EqPxuDiUbH6HQ35rBnCNIa7WO.jpg"
+    },
+    {
+        id: 4,
+        name: 'kendrick lamar',
+        date: "may 10 2025",
+        length: '2 months 3 days',
+        image: "https://as1.ftcdn.net/v2/jpg/07/46/85/18/1000_F_746851897_vIKiiA6EqPxuDiUbH6HQ35rBnCNIa7WO.jpg"
+    },
+    {
+        id: 5,
+        name: 'j cole',
+        date: "september 5 2025",
+        length: '9 months 3 days',
+        image: "https://as1.ftcdn.net/v2/jpg/07/46/85/18/1000_F_746851897_vIKiiA6EqPxuDiUbH6HQ35rBnCNIa7WO.jpg"
+    }
+]
+
+export default events
